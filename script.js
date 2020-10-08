2ee4b3203472a492fedc819df462f98f69d6cc63
Matthieu STEMPIN <stempin.matthieu@gmail.com>
2020-10-08 16:51:04 +0200

    get api, display informations and picture OK. TODO get type for 1 or 2 types. nice display

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const description = document.getElementById("description");
 
 const api = "https://pokeapi.co/api/v2/pokemon?limit=150";
 
+
 /**
  * Try to parse a response as JSON data
  */
@@ -18,7 +19,29 @@ function transformToJson (response) {
  * Clear the list of all its items
  */
 function emptyList () {
-    // ...
+    const   fields = description.querySelectorAll("dd");
+    fields.forEach((dd) => {
+        dd.textContent = "";
+    });
+    if(document.contains(document.querySelector("img"))){
+        document.querySelector("img").textContent = "";
+    }
+
+}
+
+function    createLi (loc, data) {
+    var name = document.querySelector("dd." + loc);
+    var dd = document.createElement("dd");
+   
+    switch (loc) {
+        case 'types':
+            dd.appendChild(document.createTextNode(data.types[0].type.name));
+            break;
+        default :
+            dd.appendChild(document.createTextNode(data[loc]));
+            break;
+    }
+    name.appendChild(dd);
 }
 
 /**
@@ -27,10 +50,21 @@ function emptyList () {
 function createItem (pokemon) {
     // Create a li tag
     const item = document.createElement("li");
+    
+    item.textContent = pokemon.name;
+    item.setAttribute("id", pokemon.name);
+    list.appendChild(item);
     // ...
-    fetch(pokemon.url).then(transformToJson).then((data) => {
-        // ...
-    });
+    // fetch(pokemon.url).then(transformToJson).then((data) => {        
+    // });
+    item.addEventListener('click', event => {
+        emptyList();
+        showDescription(pokemon);
+    })
+
+    document.querySelector(".close").addEventListener('click', event => {
+        hideDescription();
+    })
 }
 
 /**
@@ -44,12 +78,24 @@ function fillList (json) {
 /**
  * Fill and display the description
  */
-function showDescription (data) {
+function showDescription (pokemon) {
     description.classList.add("show");
 
-    const fields = description.querySelectorAll("dd");
-    fields.forEach((dd) => {
-        // ...
+    const   fields = description.querySelectorAll("dd");
+    const   img = document.createElement("img");
+    
+    //img.src = pokemon.
+
+    fetch(pokemon.url).then(transformToJson).then((data) => {
+        console.log(data);
+        createLi("name", data);
+        createLi("id", data);
+        createLi("weight", data);
+        createLi("height", data);
+        //TODO create function of types
+        createLi("types", data);
+        img.src = data.sprites.front_default;
+        description.appendChild(img);
     });
 }
 
@@ -58,6 +104,7 @@ function showDescription (data) {
  */
 function hideDescription () {
     description.classList.remove("show");
+    
 }
 
 // Fetch the API end-point and fill the list
